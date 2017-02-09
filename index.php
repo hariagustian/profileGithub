cc26db8c3f63c141841b4c51613d82586258df96
hari agustian <hari.agustian@hotmail.com>
2017-02-09 07:43:31 +0700

    no message

--- index.php
+++ index.php
@@ -1,35 +1,37 @@
-<?php
+<!doctype html>
+<html>
+	<head>
+		
+		<meta charset="utf-8" /> 
+		<meta http-equiv="X-UA-Compatible" content="IE=EDGE" />
+		<title>Search github profile</title>
 
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/',
-));
-
-// Our web handlers
-
-$app->get('/search', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->get('/search&user', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+		<link rel="stylesheet" type="text/css" href="/css/main.css" />
+		<script type="text/javascript" src="/js/profileGithub.js')"></script>
+				       
+	</head>
+	<body>
+	
+		<div class="site">
+			<div class="input">
+				<div class="result"> 
+					<input type="text" class="field" placeholder=" find github profile ">
+					<div class="loading"> Loading ... </div>
+					<div class="container">
+						<ul class="container-search">
+						
+						</ul>
+						<div class="container-content ">
+							<div class="close"> x </div>
+							<ul class="list-content ">
+								
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</body>
+	
+</html>
